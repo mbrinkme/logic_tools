f7dfd43ec30afb3e9753933ff288ae481c8f658f
Marco Brinkmeier <brinkmeier@posteo.de>
2018-01-11 05:16:47 +0100

    change . to + and + to /

--- lib/logic_tools/logicparse.rb
+++ lib/logic_tools/logicparse.rb
@@ -13,22 +13,21 @@ module LogicTools
     class Parser < Parslet::Parser
 
         # True / false
-        rule(:tru) { str("1") }
-        rule(:fal) { str("0") }
-        # Variable
-        # rule(:var) { match('[A-Za-uw-z]') }
+        rule(:tru) { str('1') }
+        rule(:fal) { str('0') }
         rule(:var) do
             match('[A-Za-z0-9]') |
-            str("{") >> ( match('[0-9A-Za-z]').repeat ) >> str("}")
+            str('{') >> ( match('[0-9A-Za-z]').repeat ) >> str('}')
         end
         # And operator
-        # rule(:andop) { str("&&") | match('[&\.\*^]') }
-        rule(:andop) { str(".") }
+        # rule(:andop) { str('&&') | match('[&\.\*^]') }
+        rule(:andop) { str('+') }
         # Or operator
         # rule(:orop) { match('[+|v]') }
-        rule(:orop) { str("+") }
+        rule(:orop) { str('/') }
         # Not operator
-        rule(:notop) { match('[~!]') }
+        # rule(:notop) { match('[~!-]') }
+        rule(:notop) { str('-') }
 
         # Grammar rules
         root(:expr)
@@ -37,25 +36,25 @@ module LogicTools
         rule(:andexpr) { (notexpr >> ( (andop >> notexpr) | notexpr ).repeat).as(:andexpr) }
         rule(:notexpr) { ((notop.as(:notop)).repeat >> term).as(:notexpr) }
         rule(:term) { tru.as(:tru) | fal.as(:fal) | var.as(:var) |
-                      ( str("(") >> expr >> str(")") ) }
+                      ( str('(') >> expr >> str(')') ) }
     end
 
     ## The logic tree generator from the syntax tree.
     class Transform < Parslet::Transform
 
         # Terminal rules
-        rule(:tru => simple(:tru)) { NodeTrue.new() }
-        rule(:fal => simple(:fal)) { NodeFalse.new() }
-        rule(:var => simple(:var)) do
+        rule(tru: simple(:tru)) { NodeTrue.new() }
+        rule(fal: simple(:fal)) { NodeFalse.new() }
+        rule(var: simple(:var)) do
             name = var.to_s
             name = name[1..-2] if name.size > 1 # Remove the {} if any.
             NodeVar.new(name)
         end
-        rule(:notop => simple(:notop)) { "!" }
+        rule(notop: simple(:notop)) { '-' }
 
         # Not rules
-        rule(:notexpr => simple(:expr)) { expr }
-        rule(:notexpr => sequence(:seq)) do
+        rule(notexpr: simple(:expr)) { expr }
+        rule(notexpr: sequence(:seq)) do
             expr = seq.pop
             if seq.size.even? then
                 expr
@@ -65,14 +64,14 @@ module LogicTools
         end
 
         # And rules
-        rule(:andexpr => simple(:expr)) { expr }
-        rule(:andexpr => sequence(:seq)) do
+        rule(andexpr: simple(:expr)) { expr }
+        rule(andexpr: sequence(:seq)) do
             NodeAnd.new(*seq)
         end
 
         # Or rules
-        rule(:orexpr => simple(:expr)) { expr }
-        rule(:orexpr => sequence(:seq)) do
+        rule(orexpr: simple(:expr)) { expr }
+        rule(orexpr: sequence(:seq)) do
             NodeOr.new(*seq)
         end
     end
@@ -80,7 +79,7 @@ module LogicTools
     ## The parser/gerator main fuction: converts the text in +str+ to a logic tree.
     def string2logic(str)
         # Remove the spaces
-        str = str.gsub(/\s+/, "")
+        str = str.gsub(/\s+/, '')
         # Parse the string
         return Transform.new.apply(Parser.new.parse(str))
     end
